28559f36e78665e346655958a102755aa1f39dac
MrDevIll <bellissimone@gmail.com>
2020-08-18 23:53:07 +0100

    add middleware for json exceptions

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,6 +1,11 @@
 var appMiddleware = function(app){
     // to avoid non json requests
-    
+    app.use(function (req, res, next){
+        if (!req.is('application/json')) {
+            res.status(401).send({"message":"please use json as content type"})
+        }
+        else next();
+    })
 
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,26 +2,15 @@ var appRouter = function(app) {
     
     
     app.post("/email", function(req, res) {
-        if (!req.is('application/json')) {
-            res.status(401).send({"message":"please use json as content type"})
-        } else if(!req.body.name || !req.body.email || !req.body.message) {
-            return res.status(501).send({"status": "error", "message": "missing a parameter"});
-        } else {
-            return res.send(req.body);
-        }
-    });
+         return res.send(req.body);
+        });
+
     app.post("*", function(req, res) {
         res.setHeader('Content-Type', 'application/json');
         res.status(501);
         res.send({ "message": "wrong path, please use /email" });
     });
-
-    app.all("*", function(req, res) {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(401);
-        res.send(JSON.stringify({ message: "request method not allowed" }));
-});
-
+    
 };
 
 
